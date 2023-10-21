7707e764805a025f31b66c1d67472075b29c1434
User <viljar444@gmail.com>
2023-10-21 12:31:48 +0300

    My first java class created by me

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    // main is method
+    // java is case sensitive language
+    // main is not the same as Main or mAin
     public static void main(String[] args) {
         // some useful shortcut
         // line duplication
@@ -9,6 +12,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("I like Java:");
 //        System.out.println("I`m Viljar");
 //        System.out.println("I`m Viljar");
     }
---
+++ src/main/java/org/example/MyFirstApp.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//Use code -> Refactor to reformat source code
+public class MyFirstApp {
+
+    // psvm
+    public static void main(String[] args) {
+        // use control + space to show context help
+        //sout - shortcut
+        System.out.println(555);
+        System.out.println("5 5 5");
+    }
+}
