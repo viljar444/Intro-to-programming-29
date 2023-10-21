a59b42badc26f5dee35c661c92d37e1b29528cbb
User <viljar444@gmail.com>
2023-10-21 11:33:13 +0300

    My first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // some useful shortcut
+        // line duplication
+        //mac: cmd + d
+        //windows: ctrl + d
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+//        System.out.println("I`m Viljar");
+//        System.out.println("I`m Viljar");
+    }
+}
