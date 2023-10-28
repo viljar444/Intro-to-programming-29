3f3492e7b538dd0a2015c5ef63688fbc71e7331a
User <viljar444@gmail.com>
2023-10-28 11:25:29 +0300

    do while loop - intro

---
+++ src/main/java/org/example/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        System.out.println("Do while loop");
+
+        int number = 0;
+        do {
+            // while loop is going to be executed at least once
+            System.out.println("Number is not greater than zero:)");
+        } while (number > 0);
+    }
+}
